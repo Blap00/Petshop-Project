d9eed41627435be4083835cdc1049261a4877cac
Tomy Hertz <110956323+Propheus@users.noreply.github.com>
2023-08-22 19:37:52 -0400

    AAXD

--- PetShop/src/main/java/com/littlepetshop/mvc/models/Catalogo.java
+++ PetShop/src/main/java/com/littlepetshop/mvc/models/Catalogo.java
@@ -1,5 +1,5 @@
 package com.littlepetshop.mvc.models;
-/** abers si se pqwduedeee a*/
+/** abers si seXDa*/
 public class Catalogo {
 
         public Catalogo(String nombre_producto, String descripcion, int valoraciones) {
