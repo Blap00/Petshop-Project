f939382ed457cda5d662567d68b9a6e7415b5400
GOTZEUS11DEMOR <135902871+GOTZEUS11DEMOR@users.noreply.github.com>
2023-09-08 22:45:48 -0300

    Update SolicitudController.java

--- PetShop/src/main/java/com/littlepetshop/mvc/controllers/SolicitudController.java
+++ PetShop/src/main/java/com/littlepetshop/mvc/controllers/SolicitudController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.littlepetshop.mvc.models.Solicitud;
 import com.littlepetshop.mvc.services.SolicitudService;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/solicitudes") // Ruta base para las solicitudes
 public class SolicitudController {
@@ -29,22 +31,29 @@ public class SolicitudController {
         return "solicitud.jsp"; // Retornar la vista del formulario de solicitud
     }
 
-    // Controlador para procesar el formulario de creación de solicitudes
+ // Controlador para procesar el formulario de creación de solicitudes
     @PostMapping("/crear")
-    public String crearSolicitud(@ModelAttribute("solicitud") Solicitud solicitud, BindingResult result, Model model) {
-        // Realiza la lógica para guardar la solicitud en la base de datos
-        // Puedes utilizar el servicio SolicitudService para hacerlo
+    public String crearSolicitud(@ModelAttribute("solicitud") Solicitud solicitud, BindingResult result, Model model, HttpSession session) {
+        // Verificar si el usuario está autenticado
+        boolean estaLogueado = (session.getAttribute("userId") != null);
+        if (!estaLogueado) {
+            return "redirect:/"; // Redirigir al inicio si el usuario no está autenticado
+        }
 
+        // Realizar la lógica para guardar la solicitud en la base de datos
         if (result.hasErrors()) {
             // Manejar errores de validación si es necesario
             return "solicitud.jsp"; // Retornar nuevamente el formulario en caso de errores
         } else {
-            // Guardar la solicitud y realizar cualquier otra lógica necesaria
+            // Guardar la solicitud en la base de datos
 
-            solicitudService.guardarSolicitud(solicitud);
+            Solicitud nuevaSolicitud = solicitudService.guardarSolicitud(solicitud);
 
-            // Redirigir a alguna página de confirmación o a donde desees
-            return "redirect:/solicitudes/confirmacion"; // Por ejemplo, redirigir a una página de confirmación
+            // Redirigir a una página de confirmación
+            // Puedes pasar datos adicionales a la página de confirmación si es necesario
+            model.addAttribute("solicitud", nuevaSolicitud);
+
+            return "redirect:/solicitudes/confirmacion"; // Redirigir a una página de confirmación
         }
     }
 
